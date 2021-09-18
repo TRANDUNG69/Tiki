6f81a4f1a2f270aeb57ed2aedcf88d97daff4a0d
TRANDUNG69 <trandungksnb6930082001@gmail.com>
2021-09-18 15:42:55 +0700

    Update

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -2,7 +2,12 @@ document.addEventListener("DOMContentLoaded", function(){
     let header_scroll = document.getElementById("header-scroll");
     let heigth = 2695;
     let sticky_content = document.getElementById("sticky-content");
+    let search_auto = document.getElementById("search-auto");
+    let bg_dark = document.getElementById("bg-dark");
+    console.log(search_auto);
     window.addEventListener("scroll", function(){
+        search_auto.style.display = "none";
+        bg_dark.style.display = "none";
         if(window.pageYOffset >= heigth){
             header_scroll.classList.add("sticky");
             sticky_content.classList.add("mt-150")
