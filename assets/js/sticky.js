bb3a0f0b17fd4c113bb20911e3f1eb0527489d23
TRANDUNG69 <trandungksnb6930082001@gmail.com>
2021-09-21 17:05:53 +0700

    Update

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(){
     let header_scroll = document.getElementById("header-scroll");
-    let heigth = 2695;
+    let heigth = header_scroll.offsetTop + 250;
     let sticky_content = document.getElementById("sticky-content");
     let search_auto = document.getElementById("search-auto");
     let bg_dark = document.getElementById("bg-dark");
-    console.log(search_auto);
+    let footer_container = document.getElementById("footer-container");
+    // console.log(footer_container.offsetTop);
+    // console.log(search_auto);
     window.addEventListener("scroll", function(){
         search_auto.style.display = "none";
         bg_dark.style.display = "none";
@@ -15,6 +17,12 @@ document.addEventListener("DOMContentLoaded", function(){
         else{
             header_scroll.classList.remove("sticky");
             sticky_content.classList.remove("mt-150");
+        }
+        if(window.pageYOffset >= footer_container.offsetTop - 100){
+            header_scroll.classList.add("mt-50");
+        }
+        else{
+            header_scroll.classList.remove("mt-50");
         }
     })
 }, false)
