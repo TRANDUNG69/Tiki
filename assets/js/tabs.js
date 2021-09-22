66e2cb807be81ab79b2dfd99a1bc972bdd0d8aae
TRANDUNG69 <trandungksnb6930082001@gmail.com>
2021-09-22 22:21:29 +0700

    Update slide hide 😂😂😂

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,12 +1,12 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
-const tabs_item = $$(".tab");
-const tabs_content = $$(".tab-content");
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
+const tabs_item = document.querySelectorAll(".tab");
+const tabs_content = document.querySelectorAll(".tab-content");
 tabs_item.forEach((element, index) => {
     const tab_content = tabs_content[index];
     element.onclick = function () {
-        $(".tab.active").classList.remove("active");
-        $(".tab-content.active").classList.remove("active");
+        document.querySelector(".tab.active").classList.remove("active");
+        document.querySelector(".tab-content.active").classList.remove("active");
         this.classList.add("active");
         tab_content.classList.add("active");
     };
